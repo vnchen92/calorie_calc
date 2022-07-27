bc57622b0454b84da64e2d21003d85a4e8461d51
Vivian Chen <vnchen92@gmail.com>
2022-07-27 14:32:30 -0400

    fixed subtract and add buttons

--- src/index.js
+++ src/index.js
@@ -109,10 +109,10 @@ document.addEventListener("DOMContentLoaded", () => {
         li.appendChild(liDiv);
         let add = document.createElement("img");
         add.className = "add-button"
-        add.src = "../assets/images/add_button.png";
+        add.src = "./calorie_calc/assets/images/add_button.png";
         let subtract = document.createElement("img");
         subtract.className = "subtract-button";
-        subtract.src = "../assets/images/subtract.png";
+        subtract.src = "./calorie_calc/assets/images/subtract.png";
         liDiv.appendChild(add);
         liDiv.appendChild(subtract);
 
