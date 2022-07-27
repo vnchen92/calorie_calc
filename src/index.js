38f02b3038451d6352ed2a40746fdb4dddbacf78
Vivian Chen <vnchen92@gmail.com>
2022-07-27 14:01:53 -0400

    fixed subtract and add buttons

--- src/index.js
+++ src/index.js
@@ -109,10 +109,10 @@ document.addEventListener("DOMContentLoaded", () => {
         li.appendChild(liDiv);
         let add = document.createElement("img");
         add.className = "add-button"
-        add.src = "./assets/images/add_button.png";
+        add.src = "https://cdn.discordapp.com/attachments/1001881129546158254/1001911989330776084/add_button.png";
         let subtract = document.createElement("img");
         subtract.className = "subtract-button";
-        subtract.src = "./assets/images/subtract.png";
+        subtract.src = "https://cdn.discordapp.com/attachments/1001881129546158254/1001912080162635876/subtract.png";
         liDiv.appendChild(add);
         liDiv.appendChild(subtract);
 
